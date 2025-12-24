d0f24f9aaf96a61ceb493d84610651ed5521a5f5
Tim Kelch <tim.kelch@stedin.net>
2025-12-24 10:57:10 +0100

    chaned hidden files definition in neotree

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -2,23 +2,28 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  version = "*",
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
   dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
   },
   lazy = false,
   keys = {
-    { "\\",        ":Neotree reveal<CR>", desc = "NeoTree reveal", silent = true },
-    { "<leader>e", ":Neotree reveal<CR>", desc = "NeoTree reveal", silent = true },
+    { '\\',        ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<leader>e', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
     filesystem = {
+      filtered_items = {
+        visible = false, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
       window = {
         mappings = {
-          ["\\"] = "close_window",
+          ['\\'] = 'close_window',
         },
       },
     },

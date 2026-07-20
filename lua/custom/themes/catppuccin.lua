d5e05cad5e64daedc94129e9b86308b643e15a65
helm-hamer-hand <54846467+helm-hamer-hand@users.noreply.github.com>
2026-07-20 22:55:39 +0200

    background transparancy added

--- lua/custom/themes/catppuccin.lua
+++ lua/custom/themes/catppuccin.lua
@@ -5,9 +5,15 @@ return {
     priority = 1000,
     opts = {
       flavour = 'mocha',
+      transparent_background = true,
+      float = {
+        transparent = true,
+      },
     },
     config = function(_, opts)
       require('catppuccin').setup(opts)
+      vim.o.winblend = 15
+      vim.o.pumblend = 15
       vim.cmd.colorscheme 'catppuccin-mocha'
     end,
   },
